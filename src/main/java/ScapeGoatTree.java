e04ed099de37b06998faf32e8f62ef1c0799d9d3
galievaregina <07082001uhb>
2020-12-09 18:33:10 +0300

    реализовала интерфейс Map и добавила тесты.Протестировала CodeCoverage

--- src/main/java/ScapeGoatTree.java
+++ src/main/java/ScapeGoatTree.java
@@ -1,26 +1,35 @@
 import java.util.*;
 
-public class ScapeGoatTree<T extends Comparable<T>> {
+public class ScapeGoatTree<T extends Comparable<T>> implements Map {
     private Node<T> root;
     private int maxSize = 0;// максимальный размер дерева-максимальное число вершин в дереве после последней перебалансировки
 
-    public class Node<T extends Comparable<T>> {
-        T data;
+    public static class Node<T extends Comparable<T>> {
+        T key;
+        T value;
         Node<T> right;
         Node<T> left;
 
-        public Node(T data) {
+        public Node(T data,T value) {
             this.right = null;
             this.left = null;
-            this.data = data;
+            this.key = data;
+            this.value = value;
         }
     }
+
+    public Node getNode(T key){
+        if(contains(key)){
+            return find(key);
+        }
+        return null;
+    }
+
 
     public int size() {//вес вершины(х).Кол-во всех дочерних элементов + она сама
 
         return subtreeSize(root);
     }
-
     private int subtreeSize(Node<T> node) {
         if (node == null) {
             return 0;
@@ -52,13 +61,13 @@ public class ScapeGoatTree<T extends Comparable<T>> {
      * Далее находим узел,в котором потерян баланс и перестраеваем его поддерево.
      */
     //T = O(logN)
-    public boolean add(T data) {
-        Node<T> closest = find(data);
-        int comparison = closest == null ? -1 : data.compareTo(closest.data);
+    public boolean add(T key ,T value) {
+        Node<T> closest = find(key);
+        int comparison = closest == null ? -1 : key.compareTo(closest.key);
         if (comparison == 0) {
             return false;
         }
-        Node<T> newNode = new Node<T>(data);
+        Node<T> newNode = new Node<T>(key,value);
         if (closest == null) {
             root = newNode;
         } else if (comparison < 0) {
@@ -71,7 +80,7 @@ public class ScapeGoatTree<T extends Comparable<T>> {
         maxSize++;
         double coefficient = Math.log(maxSize) / Math.log(3.0 / 2.0);//число определяющее требуемую степень балансировки
         if (this.height() > coefficient) {
-            this.balance();
+            balance();
         }
         return true;
     }
@@ -79,28 +88,30 @@ public class ScapeGoatTree<T extends Comparable<T>> {
     private void balance() {//балансировка дерева
         if (root == null) return;
         Iterator<T> iterator = this.inorderIterator();
-        List<T> list = new ArrayList<T>();
+        List<Node<T>> list = new ArrayList<>();
         while (iterator.hasNext()) {
-            list.add(iterator.next());//получаем список со всеми значениями
+            list.add(getNode(iterator.next()));//получаем список со всеми значениями
         }
         root = null;
         balancedTree(list);
     }
 
-    private void balancedTree(List<T> list) {//строим новое сбалансированное дерево
+    private void balancedTree(List<Node<T>> list) {//строим новое сбалансированное дерево
         if (list.isEmpty()) return;
         if (list.size() == 1) {
-            this.add(list.get(0));
+            Node<T> node = list.get(0);
+            this.add(node.key,node.value);
             return;
         }
         int mediana = (list.size() - 1) / 2;
-        this.add(list.get(mediana));
+        Node<T> n = list.get(mediana);
+        this.add(n.key,n.value);
         list.remove(mediana);
         //нашли медиану сделали ее корнем,после чего удаляем ее из списка
         //повторяем все тоже самое для левого и правого поддерева
-        ArrayList<T> leftList = new ArrayList<T>(list.subList(0, (list.size() - 1) / 2));
+        ArrayList<Node<T>> leftList = new ArrayList<Node<T>>(list.subList(0, (list.size() - 1) / 2));
         balancedTree(leftList);
-        ArrayList<T> rightList = new ArrayList<T>(list.subList((list.size() - 1) / 2, list.size()));
+        ArrayList<Node<T>> rightList = new ArrayList<>(list.subList((list.size() - 1) / 2, list.size()));
         balancedTree(rightList);
     }
 
@@ -114,7 +125,7 @@ public class ScapeGoatTree<T extends Comparable<T>> {
     private void inorderTraverse(Queue<T> queue, Node<T> node) {//обход дерева от конечных узлов к корню
         if (node == null) return;
         inorderTraverse(queue, node.left);
-        queue.add(node.data);
+        queue.add(node.key);
         inorderTraverse(queue, node.right);
     }
 
@@ -124,7 +135,7 @@ public class ScapeGoatTree<T extends Comparable<T>> {
     }
 
     private Node<T> find(Node<T> start, T data) {
-        int comparison = data.compareTo(start.data);
+        int comparison = data.compareTo(start.key);
         if (comparison == 0) {
             return start;
         } else if (comparison < 0) {
@@ -138,26 +149,13 @@ public class ScapeGoatTree<T extends Comparable<T>> {
 
     public boolean contains(T data) {
         Node<T> closest = find(data);
-        return closest != null && data.compareTo(closest.data) == 0;
-    }
-
-    // T=O(logN)
-    public boolean remove(T data) {
-        maxSize--;
-        boolean contains = contains(data);
-        if (contains) {
-            root = removeRecursive(root, data);
-        }
-        if (this.height() > Math.log(maxSize) / Math.log(3.0 / 2.0)) {
-            this.balance();
-        }
-        return contains;
+        return closest != null && data.compareTo(closest.key) == 0;
     }
 
     public Node<T> removeRecursive(Node<T> node, T data) {
         if (node == null) return null;
 
-        int comparison = data.compareTo(node.data);
+        int comparison = data.compareTo(node.key);
         if (comparison < 0) {
             node.left = removeRecursive(node.left, data);
             return node;
@@ -174,7 +172,7 @@ public class ScapeGoatTree<T extends Comparable<T>> {
             Node<T> right = node.right;
             Node<T> min = findSmallestValue(node.right);
             node = min;
-            node.right = removeRecursive(right, node.data);
+            node.right = removeRecursive(right, node.key);
             node.left = left;
             return node;
         } else {
@@ -184,8 +182,104 @@ public class ScapeGoatTree<T extends Comparable<T>> {
 
     }
 
-    public Node findSmallestValue(Node<T> root) {
+    public Node<T> findSmallestValue(Node<T> root) {
         return root.left == null ? root : findSmallestValue(root.left);
     }
+    @Override
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
 
+    @Override
+    public boolean containsKey(Object key) {
+        return this.keySet().contains(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return this.values().contains(value);
+    }
+
+    @Override
+    public Object get(Object key) {
+        if(contains((T)key)) return getNode((T)key).value;
+        return null;
+
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        if(this.contains((T)key)){
+            Node node =  getNode((T)key);
+            T lastValue = (T)node.value;
+            node.value = (T) value;
+            return lastValue;
+        } else{
+            add((T)key,(T)value);
+            return null;
+        }
+    }
+
+    @Override
+    // T=O(logN)
+    public Object remove(Object key) {
+        maxSize--;
+        boolean contains = contains((T)key);
+        if (contains) {
+            root = removeRecursive(root, (T)key);
+        }
+        if (this.height() > Math.log(maxSize) / Math.log(3.0 / 2.0)) {
+            this.balance();
+        }
+        return contains;
+    }
+
+    @Override
+    public void putAll(Map m) {
+        m.forEach((key, value) -> put(key,value));
+    }
+
+    @Override
+    public void clear() {
+        Iterator<T> iterator = this.inorderIterator();
+        while (iterator.hasNext()){
+            remove(iterator.next());
+        }
+    }
+
+    @Override
+    public Set keySet() {
+        if(this.size()==0) return null;
+        Iterator<T> iterator = this.inorderIterator();
+       Set<T> keySet = new HashSet<>();
+       while(iterator.hasNext()){
+           keySet.add(iterator.next());
+       }
+       return keySet;
+    }
+
+    @Override
+    public Collection values() {
+        if(this.size()==0) return null;
+        List<T> values =  new ArrayList<>();
+        Iterator<T> iterator = this.inorderIterator();
+        while(iterator.hasNext()){
+            Node<T> node = getNode(iterator.next());
+            values.add(node.value);
+        }
+        return values;
+    }
+
+    @Override
+    public Set<Entry> entrySet() {
+        if(this.size()==0) return null;
+        Iterator<T> iterator = this.inorderIterator();
+        Set<Entry> entrySet = new HashSet<>();
+        while (iterator.hasNext()){
+            Node<T> node = getNode(iterator.next());
+            AbstractMap.SimpleEntry<T, T> entry = new AbstractMap.SimpleEntry<T, T>(node.key, node.value);
+            entrySet.add(entry);
+        }
+        return entrySet;
+    }
 }
--- src/test/java/ScapeGoatTreeTest.java
+++ src/test/java/ScapeGoatTreeTest.java
@@ -1,18 +1,18 @@
 import org.junit.Test;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
 public class ScapeGoatTreeTest {
-    ScapeGoatTree<Integer> tree;
+    ScapeGoatTree<Integer> tree =  new ScapeGoatTree<Integer>() {{
+        add(0,2);
+        add(1,3);
+        add(2,5);
+        add(3,5);
+    }};
 
     @Test
     public void size() {
-        tree = new ScapeGoatTree<Integer>() {{
-            add(0);
-            add(1);
-            add(2);
-            add(3);
-        }};
         assertEquals(4, tree.size());
     }
 
@@ -20,24 +20,19 @@ public class ScapeGoatTreeTest {
     public void height() {
         tree = new ScapeGoatTree<>();
         assertEquals(-1, tree.height());
-        tree.add(7);
+        tree.add(7,6);
         assertEquals(0, tree.height());
     }
 
     ScapeGoatTree<Integer> newTree = new ScapeGoatTree<Integer>() {{
-        add(50);
-        add(42);
-        add(40);
-        add(47);
-        add(58);
-        add(55);
-        add(60);
+        add(50,50);
+        add(42,50);
+        add(40,89);
+        add(47,99);
+        add(58,0);
+        add(55,10);
+        add(60,90);
     }};
-
-    @Test
-    public void balance() {
-
-    }
 
     @Test
     public void contains() {
@@ -48,13 +43,105 @@ public class ScapeGoatTreeTest {
     @Test
     public void add() {
         assertEquals(2, newTree.height());
-        assertFalse(newTree.add(60));
+        tree.add(4,8);
+        assertEquals(2,tree.height());
+        assertFalse(newTree.add(60,90));
+    }
+
+    @Test
+    public void isEmpty() {
+        assertFalse(newTree.isEmpty());
+        ScapeGoatTree treeSimple = new ScapeGoatTree();
+        assertTrue(treeSimple.isEmpty());
+    }
+
+    @Test
+    public void containsKey() {
+        assertTrue(newTree.containsKey(40));
+        assertFalse(newTree.containsKey(2003003));
+    }
+
+    @Test
+    public void containsValue() {
+        assertFalse(newTree.containsValue(500));
+        assertTrue(newTree.containsValue(50));
+    }
+
+    @Test
+    public void put() {
+        assertEquals(50,newTree.put(50,100));
+        assertEquals(null, newTree.put(3939,848484));
 
     }
 
     @Test
-    public void remove() {
+    public void testRemove() {
         newTree.remove(42);
         assertEquals(6, newTree.size());
+    }
+
+    @Test
+    public void putAll() {
+        ScapeGoatTree treeSimple = new ScapeGoatTree();
+        Map map = new HashMap(){{
+            put(454,447477447);
+            put(88488484,999);
+            put(65656656,0);
+        }};
+        treeSimple.putAll(map);
+        assertEquals(3,treeSimple.size());
+    }
+
+    @Test
+    public void clear() {
+        newTree.clear();
+        assertEquals(0,newTree.size());
+    }
+    @Test
+    public void get(){
+        assertEquals(50,newTree.get(50));
+        assertEquals(null,newTree.get(37373773));
+    }
+
+    @Test
+    public void keySet() {
+        Set s = new HashSet(){{
+            add(50);
+            add(42);
+            add(40);
+            add(47);
+            add(58);
+            add(55);
+            add(60);
+        }};
+        assertEquals(s,newTree.keySet());
+    }
+
+    @Test
+    public void values() {
+        List<Integer> list = new ArrayList<Integer>(){{
+            add(89);
+            add(50);
+            add(99);
+            add(50);
+            add(10);
+            add(0);
+            add(90);
+        }};
+        assertEquals(list,newTree.values());
+    }
+
+    @Test
+    public void entrySet() {
+        Set<Map.Entry> set = new HashSet<Map.Entry>(){{
+            add(new AbstractMap.SimpleEntry(50,50));
+            add(new AbstractMap.SimpleEntry(42,50));
+            add(new AbstractMap.SimpleEntry(40,89));
+            add(new AbstractMap.SimpleEntry(47,99));
+            add(new AbstractMap.SimpleEntry(58,0));
+            add(new AbstractMap.SimpleEntry(55,10));
+            add(new AbstractMap.SimpleEntry(60,90));
+        }};
+        assertEquals(set,newTree.entrySet());
     }
 }
